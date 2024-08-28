12fee552e92f01531bc65ee212e144dd8218e2aa
David Pigman <david.pigman@cayeson.com>
2024-08-28 08:13:35 -0700

    Day 10 First Last Name optimized with functions

--- Day 10/First Last Name/main.py
+++ Day 10/First Last Name/main.py
@@ -1,31 +1,24 @@
 #Original working solution not optimized with functions
-def format_name(first_name, last_name):
-    first_name_len = len(first_name)
-    last_name_len = len(last_name)
-    first_name_typecase = ""
-    last_name_typecase = ""
+def format_name(name):
+    name_len = len(name)
+    name_typecase = ""
 
-    for position, letter in enumerate(first_name):
+    for position, letter in enumerate(name):
         if position == 0:
-            first_name_typecase += first_name[0].upper()
+            name_typecase += name[0].upper()
         else:
-            first_name_typecase += first_name[position].lower()
+            name_typecase += name[position].lower()
+        
+        #Debugging Statement
+        #print(f"name_typecase {name_typecase} {position}")
 
-        print(f"first_name_typecase {first_name_typecase} {position}")
-
-    for position, letter in enumerate(last_name):
-        if position == 0:
-            last_name_typecase += last_name[0].upper()
-        else:
-            last_name_typecase += last_name[position].lower()
-
-    full_name = first_name_typecase + " " + last_name_typecase
-    
-    return full_name
-
+    return name_typecase
 
 first_name = input("Enter First Name: ")
 last_name = input("Enter Last Name: ")
 
-full_name = format_name(first_name, last_name)
+first_name_typecase = format_name(first_name)
+last_name_typecase = format_name(last_name)
+
+full_name = first_name_typecase + " " + last_name_typecase
 print(f"Your typecase formatted full name is {full_name}")
