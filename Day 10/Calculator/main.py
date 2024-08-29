5f052233927ac6b5762a2fe614c79f4df96abd10
David Pigman <david.pigman@cayeson.com>
2024-08-28 19:10:16 -0700

    Day 10-Calculator

---
+++ Day 10/Calculator/main.py
@@ -0,0 +1,50 @@
+def add(n1, n2):
+    return n1 + n2
+
+# TODO 1: Write out the other 3 functions - subtract, multiply and divide.
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+# TODO 2: Add these 4 functions into a dictionary as the values. Keys = "+", "-", "*", "/"
+
+calculations = {}
+continue_calc_loop = True
+continue_calc_count = 0
+
+calculations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+print(f"calculations!! {calculations}")
+
+# TODO 3: Use the dictionary operations to perform the calculations. Multiply 4 * 8 using the dictionary.
+
+while continue_calc_loop:
+    continue_calc_count += 1
+
+    if continue_calc_count == 1:
+        first_number = int(input("What's the first number? "))
+    else:
+        if continue_calc == "y":
+            first_number = output
+        else:
+            first_number = int(input("What's the first number? "))
+
+    operation = input("Pick an operation ")
+    second_number = int(input("What's the next number "))
+
+    output = calculations[operation](first_number, second_number)
+
+    print(f"{first_number} {operation} {second_number} = {output}")
+
+    continue_calc = input(f"Type 'y' to continue calculating with {output} or type 'n' to start a new calculation: ")
+
