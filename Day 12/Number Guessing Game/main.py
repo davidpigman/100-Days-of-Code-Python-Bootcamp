f207f97e299a19d21247bdc52fa95aa46284a01a
David Pigman <david.pigman@cayeson.com>
2024-08-30 20:26:50 -0700

    Day 12 - Number Guessing Game - Optimized for functions instead of assigning values to global variables locally in functions.

--- Day 12/Number Guessing Game/main.py
+++ Day 12/Number Guessing Game/main.py
@@ -8,35 +8,49 @@ def choose_difficulty():
         return HARD_GUESSES_INT
     else: 
         return EASY_GUESSES_INT
+    
+# Function to check users' guess against actual answer
+# Track the number of turns and reduce by 1 if they get it wrong
+def check_guess(num_guess, num_of_guesses):
+    if num_guess == RANDOM_INT:
+        print(f"You got it! The answer was {RANDOM_INT}")
+    elif num_guess > RANDOM_INT:
+        print("Too high.\nGuess Again.")
+        return num_of_guesses - 1
+    elif num_guess < RANDOM_INT:
+        print("Too low.\nGuess Again.")
+        return num_of_guesses - 1
 
-def guess_number():
+# Function to check users' guess against actual answer
+
+def guess_number(num_of_guesses):
     #Function to check users guess against another
-    global NUM_OF_GUESSES
-    global RANDOM_INT
 
-    # print(f"random int {random_int}")
+    #print(f"random int {RANDOM_INT}")
 
-    print(f"You have {NUM_OF_GUESSES} attempts remaining to guess the number.")
+    print(f"You have {num_of_guesses} attempts remaining to guess the number.")
 
     num_guess = int(input("Make a guess: "))
 
     #Track the number of turns and reduce by 1 if they get it wrong
-    if num_guess == RANDOM_INT:
-        print("You won.  Game over")
-    elif num_guess > RANDOM_INT:
-        print("Too high.\nGuess Again.")
-        NUM_OF_GUESSES -= 1
-    elif num_guess < RANDOM_INT:
-        print("Too low.\nGuess Again.")
-        NUM_OF_GUESSES -= 1
+    #if num_guess == RANDOM_INT:
+    #    print("You won.  Game over")
+    #elif num_guess > RANDOM_INT:
+    #    print("Too high.\nGuess Again.")
+    #    NUM_OF_GUESSES -= 1
+    #elif num_guess < RANDOM_INT:
+    #    print("Too low.\nGuess Again.")
+    #    NUM_OF_GUESSES -= 1
+
+    num_of_guesses = check_guess(num_guess, num_of_guesses)
 
     if num_guess == RANDOM_INT:
         print("")
-    elif NUM_OF_GUESSES == 0:
+    elif num_of_guesses == 0:
         print("No more guesses left, you lose!")
     else:
         #Repeat the guessing functionality if they get it wrong
-        guess_number()
+        guess_number(num_of_guesses)
 
 MAXIMUM_NUMBER = 100
 RANDOM_INT = random.randrange(1, 100, 1)
@@ -49,10 +63,10 @@ print("I'm thinking of a number between 1 and 100.")
 
 #Function to set difficulty
 #difficulty = input("Choose a difficulty. Type 'easy' or 'hard' ")
-NUM_OF_GUESSES = choose_difficulty()
+num_of_guesses = choose_difficulty()
 
 #Let the user guess a number
-guess_number()
+guess_number(num_of_guesses)
 
 
 
